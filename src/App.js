f5b7a96bfac9d5ea92b8cc1da93ad87d5adcffeb
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2025-07-08 12:51:11 +0530

    completed Todo List and learned about useState

--- src/App.js
+++ src/App.js
@@ -3,27 +3,60 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-// const[text,setText]=useState("")
-const [showtext, setShowText] = useState(false);
-// const changeText=(event)=>{
-//   setText(event.target.value)
-// }
-const changeText=()=>{
-  setShowText(!showtext)
+const[todoList,setTodoList]=useState([])
+const[newTask,setNewTask]=useState("")
+const handleChange=(event)=>{
+setNewTask(event.target.value)
 }
+const addTask=()=>{
+  const task={
+    id :todoList.length===0 ? 1 :todoList[todoList.length-1].id+1,
+    taskName: newTask,
+    complete: false
+  }
+const newTodoList=[...todoList,task]
+setTodoList(newTodoList)
+}
+const deleteTask=(id)=>{
+const newTodoList=todoList.filter((task)=> task.id!==id)
+setTodoList(newTodoList);
+}
+
+const updateTask=(id)=>{
+setTodoList(
+  todoList.map((task)=>{
+    if(task.id===id){
+      return{...task, complete:true}
+    }else{
+            return task
+    }
+  }
+  )
+)
+}
+
   return (
     <>
-    {/* <input type="text" onChange={changeText} palceholder="name"/> */}
-    {/* {text} */}
-
-    {/* onclick state change */}
-<button onClick={changeText}>show/hide</button>
-{showtext&& <h1>Hi My name is AK</h1>}
-
+      <div className="App">
+        <div className="addTask">
+          <input onChange={handleChange} />
+          <button onClick={addTask}>Add Task</button>
+        </div>
+        <div className="list">
+          {todoList.map((task) => {
+            return (
+              <div>
+                <h1>{task.taskName}</h1>
+                <button onClick={() => deleteTask(task.id)}>X</button>
+                <button onClick={updateTask}>Complete</button>
+              </div>
+            );
+          })}
+        </div>
+      
+      </div>
     </>
-  
-  )
-
+  );
 }
 
 
