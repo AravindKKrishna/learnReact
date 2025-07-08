e75f74dd00b04c41803c99933febd079d2b08296
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2025-07-08 22:58:31 +0530

    Learned useEffect with mounting updating unmounting

--- src/App.js
+++ src/App.js
@@ -1,60 +1,24 @@
 
 import './App.css';
-import { useState } from 'react';
+import { useState ,useEffect } from 'react';
 
 function App() {
-const[todoList,setTodoList]=useState([])
-const[newTask,setNewTask]=useState("")
-const handleChange=(event)=>{
-setNewTask(event.target.value)
-}
-const addTask=()=>{
-  const task={
-    id :todoList.length===0 ? 1 :todoList[todoList.length-1].id+1,
-    taskName: newTask,
-    complete: false
+const[count,setCount]=useState(0)
+useEffect(() => {
+  console.log("mounting");
+  return ()=>{
+    console.log("unmounting");
+    
   }
-const newTodoList=[...todoList,task]
-setTodoList(newTodoList)
-}
-const deleteTask=(id)=>{
-const newTodoList=todoList.filter((task)=> task.id!==id)
-setTodoList(newTodoList);
-}
-
-const updateTask=(id)=>{
-setTodoList(
-  todoList.map((task)=>{
-    if(task.id===id){
-      return{...task, complete:true}
-    }else{
-            return task
-    }
-  }
-  )
-)
-}
+},[]);
+ const changeCount=()=>{
+  setCount(count+1)
+ }
 
   return (
     <>
-      <div className="App">
-        <div className="addTask">
-          <input onChange={handleChange} />
-          <button onClick={addTask}>Add Task</button>
-        </div>
-        <div className="list">
-          {todoList.map((task) => {
-            return (
-              <div>
-                <h1>{task.taskName}</h1>
-                <button onClick={() => deleteTask(task.id)}>X</button>
-                <button onClick={updateTask}>Complete</button>
-              </div>
-            );
-          })}
-        </div>
-      
-      </div>
+      <button onClick={changeCount}>clickme</button>
+      {count}
     </>
   );
 }
