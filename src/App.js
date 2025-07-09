c2fa1dc533540ec8a1f756f154dd951d7cc86be2
Aravind K Krishna <ananthkkrishna2001@gmail.com>
2025-07-09 09:59:12 +0530

    Learned Api fetch using fetch and axios with  useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 
 import './App.css';
+import Axios from "axios"
 import { useState ,useEffect } from 'react';
 
+//if we use axios it will keep fetching data everytime when a component renders thus it become an infinite loop.
+// so to avoid that we use  useEffect and put the axios code in useEffect
+
 function App() {
-const[count,setCount]=useState(0)
-useEffect(() => {
-  console.log("mounting");
-  return ()=>{
-    console.log("unmounting");
-    
-  }
-},[]);
- const changeCount=()=>{
-  setCount(count+1)
- }
+  // //using fetch
+  // fetch("https://catfact.ninja/fact")
+  //   .then((res)=>res.json())
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
+
+  const [catFact, setcatFact] = useState("");
+// useEffect(()=>{
+//   //using Axios Library
+//   Axios.get("https://catfact.ninja/fact").then((res) => {
+//     setcatFact(res.data.fact);
+//   });
+// },[])
+
+
+//this represt how an api is called while clicking button and here we use useEffect to display the button function and it helps to prevent the infinite loop 
+useEffect(()=>{
+  fetchCatFact()
+},[])
+
+const fetchCatFact=()=>{
+  Axios.get("https://catfact.ninja/fact").then((res) => {
+       setcatFact(res.data.fact);
+       });
+}
 
   return (
     <>
-      <button onClick={changeCount}>clickme</button>
-      {count}
+    <button onClick={fetchCatFact}>Generate cat fact</button>
+    <p>{catFact}</p>
     </>
   );
 }
